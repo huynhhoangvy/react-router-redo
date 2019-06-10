ee23b09c716497b458a3135cf9d1983c3c61ecad
huynhhoangvy <huynh.h.vy@gmail.com>
2019-06-11 03:42:48 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+const data = require ("../src/data.json");
+console.log(data)
+
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homePage">
+      Homepage
     </div>
-  );
+  )
+}
+
+class Candidates extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state={
+      data : data,
+    }
+  }
+
+  EditCandidate = (id) => {
+    this.props.history.push(`candidates/edit/${id}`)
+    console.log("thisthisthisthis", this.props.history)
+  }
+
+  RenderCandidates = () => {
+    return (
+      this.state.data.map(({first_name, last_name, id, avatar, gender}) => {
+        return (
+          <div key={id}>
+            <img src={avatar} />
+            <p>Full Name: {first_name + " " + last_name} <br />
+            Gender: {gender}</p>
+            <button onClick={() => this.RemoveCandidate(id)} >Remove Candidate</button>
+            <button onClick={() => this.EditCandidate(id)} >Edit Candidate</button>
+            <Link to={"/candidates/view/" + id} >Details...</Link>
+            <hr />
+          </div>
+        )
+      })
+    )
+  }
+
+  RemoveCandidate = (id) => {
+    const newData = this.state.data.filter (element => {
+      return element.id !== id
+    })
+
+    this.setState ({data: newData})
+
+    console.log ("newdatalength", newData.length)
+  }
+
+  render () {
+    return (
+      <div className="candidates">
+        {this.RenderCandidates()}
+      </div>
+    )
+  }
+}
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <ul style={{margin: "0px", listStyleType: "none"}}>
+        <li><Link to="/" >Home</Link></li>
+        
+        
+        <li><Link to="/candidates" >Candidates</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+const EditCandidatePage = () => {
+  return (
+    <div className="eidtCandidatePage">
+      Edit Candidate Page
+    </div>
+  )
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state={
+      id: 123,
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/candidates" exact component={Candidates} />
+          <Route path="/candidates/edit/:id" exact component={() => <EditCandidatePage isAuthed={true} newId={this.state.id} />} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
